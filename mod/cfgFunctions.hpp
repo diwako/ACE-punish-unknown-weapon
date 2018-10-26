8ec8470f6c48a0e923d8d5ec00af9c402646989c
diwako <diwako@gmail.com>
2018-10-26 19:02:21 +0200

    fix function declaration

--- mod/cfgFunctions.hpp
+++ mod/cfgFunctions.hpp
@@ -1,9 +1,11 @@
-class diwako_unknownwp {
-  tag="diwako_unknownwp";
-  class functions {
-    file = "diwako_unknownwp\functions\diwako\unknownwp";
-    class firedEh;
-    class reloadedEh;
-    class jammedEh;
+class cfgFunctions {
+  class diwako_unknownwp {
+    tag="diwako_unknownwp";
+    class functions {
+      file = "diwako_unknownwp\functions\diwako\unknownwp";
+      class firedEh;
+      class reloadedEh;
+      class jammedEh;
+    };
   };
 };
